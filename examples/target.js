91b552ad2ff9012a5098ee13581c5f0aa2e86db3
takashi kudo <takudo@mx1.wiseman.co.jp>
2021-03-16 18:16:36 +0900

    first commit

---
+++ examples/target.js
@@ -0,0 +1,9 @@
+Promise.resolve().then(function () {
+<<<<<<< HEAD
+    console.log('target.js');
+=======
+    console.debug('target.js');
+>>>>>>> branch-name
+}).catch(function (error) {
+    console.error(error);
+})
